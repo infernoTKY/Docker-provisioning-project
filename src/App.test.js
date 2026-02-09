3bf69d89b663acbee0897df598a93310bde2a43a
infernoTKY <bentan249@gmail.com>
2026-02-09 11:39:32 +0800

    Changed App test to include Dev and Prod

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const originalEnv = process.env;
+
+beforeEach(() => {
+  jest.resetModules();
+  process.env = { ...originalEnv };
 });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterAll(() => {
+  process.env = originalEnv;
 });
 
+test('renders Development Server text by default', () => {
+  process.env.REACT_APP_NAME = 'development';
+  render(<App />);
+  expect(screen.getByText(/Development Server/i)).toBeInTheDocument();
+  const banner = screen.queryByText(/Production Environment/i);
+  expect(banner).not.toBeInTheDocument();
+});
+
+test('renders Production Banner when env is production', () => {
+  process.env.REACT_APP_NAME = 'production';
+  render(<App />);
+  expect(screen.getByText(/Production Environment/i)).toBeInTheDocument();
+  expect(screen.getByText(/This is the Production Server/i)).toBeInTheDocument();
+});
+
+// The new test you want to add
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
